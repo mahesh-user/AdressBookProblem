e3039bffbcd4e4718c870b0275ee2b2d9714c8a9
Mahesh <mahesh51138@gmail.com>
2022-03-06 18:33:34 +0530

    Added:Create Contact Details

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        Contact contact = new Contact("mahesh","NimkampalliRoad","anantapur","AP","makam95@gmail.com",
+                856321545,515201);
+    }
+}
---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class Contact {
+
+    String name, address, city, state, email;
+    long phoneNumber, zipCode;
+
+
+    public Contact(String name, String address, String city, String state, String email , long phoneNumber, long zipcode) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.zipCode = zipcode;
+    }
+}
